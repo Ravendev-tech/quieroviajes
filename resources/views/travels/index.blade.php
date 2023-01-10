24fba04d7b216d60eebf7b4662eb3c21289aa170
Ravendev-tech <damian.g.gutman@gmail.com>
2023-01-10 10:53:23 +0100

    asd

--- resources/views/travels/index.blade.php
+++ resources/views/travels/index.blade.php
@@ -75,7 +75,29 @@
                   @endif
                 </th>
                 @if((Auth::user()->user_level) == 1)
-                <th><a href="{{route('travels.destroy',$expedientesItem->localizador)}}"><i class="lni lni-trash"></i></a> </th>
+                <th><a href="#" data-bs-toggle="modal" data-bs-target="#deletemodal{{$expedientesItem->localizador}}"><i class="lni lni-trash"></i></a> </th>
+                <!-- modal -->
+                <div class="modal fade" id="deletemodal{{$expedientesItem->localizador}}" tabindex="-1" aria-hidden="true">
+                  <div class="modal-dialog modal-dialog-centered">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h5 class="modal-title">Eliminar</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                      </div>
+                      <div class="modal-body">¿Deseas Eliminar este Expediente?</div>
+                      <div class="modal-footer">
+                        <form class="" action="{{route('travels.destroy',$expedientesItem->localizador)}}" method="get">
+                          @method('post')
+                          @csrf
+                          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                          <button type="submit" class="btn btn-danger">Eliminar</button>
+                        </form>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <!-- endmodal -->
+
                 @else
                 @endif
               </tr>
